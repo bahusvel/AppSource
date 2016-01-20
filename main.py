8e309107bafaa99e32ac1d61d34c24ba8370c30c
Denis Lavrov <bahus.vel@gmail.com>
2016-01-20 15:14:48 +1300

    Fetching Done

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ import subprocess
 import os
 import re
 
+
+def findFileByType(appcontent, extension):
+	for file in appcontent:
+		if file.endswith(extension):
+			return file
+	return None
+
 BUILDPATH = "/Users/denislavrov/Documents/AppSourcePath"
 REPO = "https://github.com/amitburst/HackerNews.git"
+BUNDLEGROUP = "com.bahus"
 
 git = subprocess.Popen(("git", "clone", REPO), cwd=BUILDPATH, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 gitout, giterr = git.stdout.read(), git.stderr.read()
@@ -25,4 +33,16 @@ if 'Podfile' in appcontent:
 	print("You app uses CocoaPods, installing the required pods now")
 	os.chdir(BUILDPATH + "/" + gitapp)
 	os.system("pod install")
+	appworkspace = findFileByType(appcontent, ".xcworkspace")
+	appproject = findFileByType(appcontent, ".xcodeproj")
+	# Change bundle ID
+	print(appproject)
+	exit(0)
+	#useful command to get schemes in workspaces
+	#os.system("xcodebuild -list -workspace " + apptarget)
+	os.system("xcodebuild -workspace " + appworkspace + " -scheme " + gitapp)
+elif 'Cartfile' in appcontent:
+	print("Carthage not supported yet, sorry I will add support soon :)") # TODO
+else:
+	print("No dependency manager detected") # TODO build from .project
 
